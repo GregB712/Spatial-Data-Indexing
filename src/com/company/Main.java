54d046cd8691b35e3c949c4f79770ebb901fadb7
GregB712 <gregorybarbas@gmail.com>
2020-10-03 20:27:05 +0300

    CSV Parser for datafile creation.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,15 +10,19 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
+        //Create Datafile from csv
+        CSVParser csvParser = new CSVParser(2, "outfile.csv");
+        csvParser.CSVParsing();
+        csvParser.writeDataFile();
         // Initialise R-Tree and build it
-        RTree rtree = new RTree(2,"test.csv");
-        rtree.BuildRTree();
-        rtree.WriteIndexFile();
+        //RTree rtree = new RTree(2,"test.csv");
+        //rtree.BuildRTree();
+        //rtree.WriteIndexFile();
 
         /** Queries for R-tree  **/
         //rtree.InsertNewEntry(2);          // (DONE!) Inserts new entry into the tree
 
-        //rtree.RangeQuery(2);              // (DONE!) Prints the datafile line for every entry that belongs to the query
+        //rtree.RangeQuery(2);              // (DONE!) Prints the old_datafile line for every entry that belongs to the query
 
         //rtree.kNNQuery(2,3);              // (NOT DONE!) TODO
 
